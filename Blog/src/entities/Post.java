e406a743adb6b1683f5deb69487d5e08e7a3fe5f
Mauricio Dittrichi <mauraumdev@gmail.com>
2019-10-23 12:51:58 -0300

    Tentativa de rodar a funçao recursiva para entrar nas pastas.

--- Blog/src/entities/Post.java
+++ Blog/src/entities/Post.java
@@ -83,7 +83,7 @@ public class Post {
 		sb.append(content + "\n");
 		sb.append("Comments: \n ");
 		for (Comment c : comments) {
-			sb.append(c.getText());
+			sb.append(c.getText() + "\n");
 		}
 		
 		return sb.toString();
