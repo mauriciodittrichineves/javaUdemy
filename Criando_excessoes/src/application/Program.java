fac43c175e818326d990cc4ee59ccd45790d407e
Mauricio Dittrichi <mauraumdev@gmail.com>
2019-10-29 16:30:28 -0300

    Realizado o tratamento de excessões na classe segunda forma a ruim

--- Criando_excessoes/src/application/Program.java
+++ Criando_excessoes/src/application/Program.java
@@ -10,48 +10,41 @@ import model.entities.Reservation;
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
-		
+
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
+
 		System.out.println("Room number");
 		int roomNumber = sc.nextInt();
 		System.out.println("Check-in");
 		Date checkIn = sdf.parse(sc.next());
 		System.out.println("Check-out");
 		Date checkOut = sdf.parse(sc.next());
-		
-		if (!checkOut.after(checkIn) ) {
+
+		if (!checkOut.after(checkIn)) {
 			System.out.println("Error in Reservation: Check-out date must be after check-in date.");
-		}else {
+		} else {
 			Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
 			System.out.println(reservation);
-			
+
 			System.out.println();
 			System.out.println("Enter data to update the reservation: ");
 			System.out.println("Check-In Date: (dd/MM/yyyy)");
 			checkIn = sdf.parse(sc.next());
 			System.out.println("Check-Out Date: (dd/MM/yyyy)");
 			checkOut = sdf.parse(sc.next());
-			
-			Date now = new Date();
-			if(checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation Dates for UPDATE must be future Dates.");
-			} else if (checkOut.before(checkIn)) {
-				System.out.println("Error in Reservation: Check-out date must be after check-in date.");
-				
-			} else {
-					
-			reservation.updateDates(checkIn, checkOut);
-			System.out.println(reservation);
+
+			String error = reservation.updateDates(checkIn, checkOut);
+
+			if (error != null) {
+				System.out.println(error);
+			}else {
+				System.out.println("Reservation: " + reservation);
 			}
-			
 		}
-		
-		
+
 		sc.close();
-		
-		
+
 	}
 
 }
--- Criando_excessoes/src/model/entities/Reservation.java
+++ Criando_excessoes/src/model/entities/Reservation.java
@@ -10,6 +10,7 @@ public class Reservation {
 	private Date checkIn;
 	private Date checkOut;
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
 	public Reservation() {
 	};
 
@@ -39,15 +40,26 @@ public class Reservation {
 		long diff = checkOut.getTime() - checkIn.getTime();
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
-	public void updateDates (Date checkIn, Date checkOut) {
+
+	public String updateDates(Date checkIn, Date checkOut) {
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "Error in reservation: Reservation Dates for UPDATE must be future Dates.";
+		}
+		if (checkOut.before(checkIn)) {
+			return "Error in Reservation: Check-out date must be after check-in date.";
+		}
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+		return null;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Room"+ roomNumber + ", check-in:  " + sdf.format(checkIn) + ",  check-out: " + sdf.format(checkOut) + " " + duration() + "   nights "
-		;
+		return "Room" + roomNumber + ", check-in:  " + sdf.format(checkIn) + ",  check-out: " + sdf.format(checkOut)
+				+ " " + duration() + "   nights ";
 	}
 
 }
